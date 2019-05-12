ce2701f52582d97b7dcf643afa44a682d6769550
Pankaj Choudhary <pankaj1712.iitr@gmail.com>
2019-05-13 00:09:54 +0530

    showing how profiles work

--- src/main/java/com/example/demo/SpringitApplication.java
+++ src/main/java/com/example/demo/SpringitApplication.java
@@ -1,14 +1,30 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import javax.swing.*;
 
 @SpringBootApplication
 public class SpringitApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
 		System.out.println("Welcome to springit!!!");
+	}
+
+	@Bean
+	@Profile("dev") 					// means this method will only be called if the profile is dev
+	CommandLineRunner runner() {
+		return args -> {
+			System.out.println("This is something that we would only do in dev");
+		};
 	}
 
 }
---
+++ src/main/java/com/example/demo/SpringitProperties.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("springit")
+public class SpringitProperties {
+
+    /**
+     * This is our welcome message
+     */
+    private String welcomeMsg = "Hello, world";
+
+    public String getWelcomeMsg() {
+        return welcomeMsg;
+    }
+
+    public void setWelcomeMsg(String welcomeMsg) {
+        this.welcomeMsg = welcomeMsg;
+    }
+
+}
